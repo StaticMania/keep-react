e0f739e4db7af4ca02a469bceb1258450027daec
Arifulislam5577 <arifalamm33@gmal.com>
2023-08-22 10:54:39 +0600

    Added: checkbox group component docs added

--- app/(docs)/checkboxGroup/page.tsx
+++ app/(docs)/checkboxGroup/page.tsx
@@ -4,7 +4,7 @@ import CheckboxGroupDocs from ".";
 
 export const metadata: Metadata = {
   description:
-    "Checkbox Group component enables users to select multiple options from a predefined list using checkboxes. It streamlines the process of selecting multiple items by presenting them together and allowing users to make their choices simultaneously. The Checkbox Group component offers various options for customization, including layout, styling, and handling user interactions.",
+    "A Checkbox Group is a user interface element that allows users to select multiple options from a list by checking corresponding checkboxes. This component is often used when users need to choose from a set of related options, and they can select one or more options simultaneously.",
   title: "Checkbox Group - Keep Design System",
 };
 
