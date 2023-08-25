2098c25d5071c2d70052824b17dc93a34aebe5c3
Arifulislam5577 <arifalamm33@gmal.com>
2023-08-25 19:40:48 +0600

    Fixed: Avatar responsive issue fixed

--- app/docs/components/avatar/variant/ImageOfAvatar.tsx
+++ app/docs/components/avatar/variant/ImageOfAvatar.tsx
@@ -4,10 +4,7 @@ const ImageOfAvatar = () => {
   return (
     <div className="flex items-center gap-3">
       <Avatar shape="round" size="xl" img="/images/avatar/avatar-4.png" />
-      <Avatar shape="round" size="xl" img="/images/avatar/avatar-4.png" />
       <Avatar shape="roundSquare" size="xl" img="/images/avatar/avatar-4.png" />
-      <Avatar shape="roundSquare" size="xl" img="/images/avatar/avatar-4.png" />
-      <Avatar shape="square" size="xl" img="/images/avatar/avatar-4.png" />
       <Avatar shape="square" size="xl" img="/images/avatar/avatar-4.png" />
     </div>
   );
