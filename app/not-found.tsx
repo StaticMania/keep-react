555186033817ec0f1e785546299ed459cdad4a1e
Arifulislam5577 <arifalamm33@gmail.com>
2023-09-01 11:25:45 +0600

    Added: 404 page added

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Empty } from "@/src/components/Empty";
+
+const NotFound = () => {
+  return (
+    <Empty
+      title="Sorry, No Result Found!"
+      content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry."
+      redirectUrl="/"
+      iconType="empty_result_1"
+    />
+  );
+};
+
+export default NotFound;
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <section className="text-gray-600 body-font">
       <div className="container">
         <h1 className="text-5xl font-bold text-center max-w-5xl mx-auto py-20">
-          GET OUT FROM THIS PAGE
+          HOME PAGE
         </h1>
       </div>
     </section>
