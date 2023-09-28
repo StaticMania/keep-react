475682283924f7a9d16170dd0073be2f05ca7dfc
Arifulislam5577 <arifalamm33@gmail.com>
2023-09-28 12:23:14 +0600

    Fixed: Radio Icon not showing issue fixed

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -54,17 +54,17 @@ const ToggleComponent: FC<ToggleProps> = ({
   const showWithIcon = (enabled: boolean, size: keyof ToggleSizes): string => {
     if (enabled) {
       const enableIcon: ToggleSizes = {
-        sm: `after:h-[10px] after:w-[10px] after:bg-[url('/images/icon/moon-10.svg')]`,
-        md: `after:h-[12px] after:w-[12px] after:bg-[url('/images/icon/moon-12.svg')]`,
-        lg: `after:h-[13px] after:w-[13px] after:bg-[url('/images/icon/moon-13.svg')]`,
+        sm: `after:h-[10px] after:w-[10px] after:bg-[url('https://staticmania.cdn.prismic.io/staticmania/94b4a4f9-0d62-4504-8ce7-1e034cd4ef4b_moon-10.svg')]`,
+        md: `after:h-[12px] after:w-[12px] after:bg-[url('https://staticmania.cdn.prismic.io/staticmania/33d8fbe6-f91d-47fb-bdb2-cf4c0ed5e233_moon-12.svg')]`,
+        lg: `after:h-[13px] after:w-[13px] after:bg-[url('https://staticmania.cdn.prismic.io/staticmania/e92c83b0-1163-4dc0-b639-3fc572208a88_moon-13.svg')]`,
       };
       return enableIcon[size];
     }
 
     const disabledIcon: ToggleSizes = {
-      sm: `after:h-[10px] after:w-[10px] after:bg-[url('/images/icon/sun-10.svg')]`,
-      md: `after:h-[12px] after:w-[12px] after:bg-[url('/images/icon/sun-12.svg')]`,
-      lg: `after:h-[13px] after:w-[13px] after:bg-[url('/images/icon/sun-13.svg')]`,
+      sm: `after:h-[10px] after:w-[10px] after:bg-[url('https://staticmania.cdn.prismic.io/staticmania/0ec60af9-5904-46c0-aa2a-c030c87a4ef9_sun-10.svg')]`,
+      md: `after:h-[12px] after:w-[12px] after:bg-[url('https://staticmania.cdn.prismic.io/staticmania/50fbbf0c-a842-464e-8f3a-c34d3b4cbc42_sun-12.svg')]`,
+      lg: `after:h-[13px] after:w-[13px] after:bg-[url('https://staticmania.cdn.prismic.io/staticmania/8bfd02e9-f130-410e-9a4f-f54eec33f14a_sun-13.svg')]`,
     };
     return disabledIcon[size];
   };
