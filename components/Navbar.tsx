47bfb1a7ae94d7fc2d71db65a996660c903fbece
Arifulislam5577 <arifalamm33@gmail.com>
2023-11-03 20:07:35 +0600

    Update: Home Page Update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -56,22 +56,32 @@ const Navbar = () => {
           {showModal && (
             <Search setShowMainModal={setShowMainModal} showModal={showModal} />
           )}
-          <div className="lg:flex items-center gap-5 hidden">
-            <button onClick={handleModal}>
+          <div className="lg:flex items-end gap-3 hidden">
+            <button
+              onClick={handleModal}
+              className="p-3 hover:bg-primary-50 bg-primary-25 rounded"
+            >
               <span>
-                <MagnifyingGlass size={22} color="#999" />
+                <MagnifyingGlass size={20} color="#455468" />
               </span>
             </button>
-
-            <a href="https://github.com/StaticMania/keep-react" target="_blank">
+            <a
+              href="https://github.com/StaticMania/keep-react"
+              target="_blank"
+              className="p-3 hover:bg-primary-50 bg-primary-25 rounded"
+            >
               <Image
                 src="/images/icon/github.svg"
-                height={22}
-                width={22}
+                height={20}
+                width={20}
                 alt="github"
               />
             </a>
-            <a href="https://keep-storybook.vercel.app" target="_blank">
+            <a
+              href="https://keep-storybook.vercel.app"
+              target="_blank"
+              className="p-3 hover:bg-primary-50 bg-primary-25 rounded"
+            >
               <svg
                 stroke="#FF4785"
                 fill="#FF4785"
@@ -92,25 +102,45 @@ const Navbar = () => {
               get started
             </Link>
           </div>
-          <div className="lg:hidden flex items-center justify-between gap-4">
-            <button onClick={handleModal}>
+          <div className="lg:hidden flex items-center justify-between gap-3">
+            <button
+              onClick={handleModal}
+              className="p-2.5 hover:bg-primary-50 bg-primary-25 rounded"
+            >
               <span>
-                <MagnifyingGlass size={20} color="#999" />
+                <MagnifyingGlass size={20} color="#455468" />
               </span>
             </button>
-            {showModal && (
-              <Search
-                setShowMainModal={setShowMainModal}
-                showModal={showModal}
-              />
-            )}
-            <a href="https://github.com/StaticMania/keep-react" target="_blank">
+
+            <a
+              href="https://github.com/StaticMania/keep-react"
+              target="_blank"
+              className="p-2.5 hover:bg-primary-50 bg-primary-25 rounded"
+            >
               <Image
                 src="/images/icon/github.svg"
-                height={24}
-                width={24}
+                height={20}
+                width={20}
                 alt="github"
               />
+            </a>
+            <a
+              href="https://keep-storybook.vercel.app"
+              target="_blank"
+              className="p-2.5 hover:bg-primary-50 bg-primary-25 rounded"
+            >
+              <svg
+                stroke="#FF4785"
+                fill="#FF4785"
+                strokeWidth="0"
+                role="img"
+                viewBox="0 0 24 24"
+                height="20px"
+                width="20px"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M16.71.243l-.12 2.71a.18.18 0 00.29.15l1.06-.8.9.7a.18.18 0 00.28-.14l-.1-2.76 1.33-.1a1.2 1.2 0 011.279 1.2v21.596a1.2 1.2 0 01-1.26 1.2l-16.096-.72a1.2 1.2 0 01-1.15-1.16l-.75-19.797a1.2 1.2 0 011.13-1.27L16.7.222zM13.64 9.3c0 .47 3.16.24 3.59-.08 0-3.2-1.72-4.89-4.859-4.89-3.15 0-4.899 1.72-4.899 4.29 0 4.45 5.999 4.53 5.999 6.959 0 .7-.32 1.1-1.05 1.1-.96 0-1.35-.49-1.3-2.16 0-.36-3.649-.48-3.769 0-.27 4.03 2.23 5.2 5.099 5.2 2.79 0 4.969-1.49 4.969-4.18 0-4.77-6.099-4.64-6.099-6.999 0-.97.72-1.1 1.13-1.1.45 0 1.25.07 1.19 1.87z"></path>
+              </svg>
             </a>
             <button onClick={() => setActive(!active)}>
               {active ? (
