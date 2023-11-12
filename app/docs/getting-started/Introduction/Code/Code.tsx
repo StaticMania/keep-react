a2646308cbead32093982723706e5475d1001ea2
rakibrahman <md.rr.talukder@gmail.com>
2023-11-12 11:29:45 +0600

    more descriptive keep-react package installtion step, added code for pnpm pack manager

--- app/docs/getting-started/Introduction/Code/Code.tsx
+++ app/docs/getting-started/Introduction/Code/Code.tsx
@@ -39,6 +39,8 @@ const twNpmInstall = 'npm i autoprefixer postcss tailwindcss\nnpx tailwindcss in
 
 const keepNpmInstall = 'npm i keep-react'
 const keepYarnInstall = 'yarn add keep-react'
+const keepPnpmInstall = 'pnpm add keep-react'
+
 
 const twConfigForNextJs = `
 module.exports = {
@@ -76,5 +78,5 @@ export {
   twNpmInstall,
   keepNpmInstall,
   twConfigForNextJs,
-  keepYarnInstall,viteReact,nextJsInstall,NextJsQnA
+  keepYarnInstall,viteReact,nextJsInstall,NextJsQnA,keepPnpmInstall
 }
