44bd4445a8b3fad3c5fb473e0675a7f8c929c9f2
Mehedi H Sajib <48852166+mehedihsajib@users.noreply.github.com>
2023-11-17 20:17:52 +0600

    mod: get back to blue image

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,7 +10,14 @@ const NotFound = () => {
         title="Sorry, No Result Found!"
         content="The page/topic you are looking for is not found."
         redirectUrl="/"
-        image={<Image src="/images/not-found.svg" height={234} width={350} alt="404" />}
+        image={
+          <Image
+            src="https://staticmania.cdn.prismic.io/staticmania/a8befbc0-90ae-4835-bf37-8cd1096f450f_Property+1%3DSearch_+Property+2%3DSm.svg"
+            height={234}
+            width={350}
+            alt="404"
+          />
+        }
       />
       <div className="-mt-8 text-center">
         <Link
