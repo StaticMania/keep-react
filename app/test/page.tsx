9b3c0382c26f17c7865a626edc8133e7b9ea7e96
Arifulislam5577 <arifalamm33@gmail.com>
2023-11-28 08:03:27 +0600

    Popover

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+'use client'
+import { Popover } from './Popover/Popover'
 
 const page = () => {
-  return <div>Testing</div>
+  return (
+    <div className="container py-20 text-metal-400">
+      <p>
+        around 170 million people holds a general election on January 7, with Prime Minister
+        <Popover>Sheikh Hasina</Popover> eyeing her fourth consecutive term in power. range of violent crimes.
+      </p>
+    </div>
+  )
 }
 
 export default page
