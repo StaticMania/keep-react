6c9c336df91d7657e4b5ab3f609d8574bebfeb2b
Md Ariful Islam <73895992+Arifulislam5577@users.noreply.github.com>
2023-12-31 19:01:28 +0600

    Keep/update conduct email (#125)
    * Updated: Update conduct email & Commit rule.
    
    * Added: New commit lint config.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  // extends: ['@commitlint/config-conventional'],
   extends: [],
   rules: {
     'header-min-length': [2, 'always', 14],
@@ -12,7 +13,7 @@ module.exports = {
           return [/^[A-Z]/.test(raw), 'Commit message must start with a capital letter']
         },
         'header-end-period': ({ header }) => {
-          return [/\.$/.test(header), 'Commit message must end with a period']
+          return [/\.$/.test(header), 'Commit message must end with a dot(.).']
         },
       },
     },
