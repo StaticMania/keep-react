1eb64d327717b3ed6686e18e630e419e1167dd89
Arifulislam5577 <arifalamm33@gmail.com>
2024-02-12 18:50:05 +0600

    Updated: Rating component updated.

--- app/docs/components/rating/variant/RatingTypes.tsx
+++ app/docs/components/rating/variant/RatingTypes.tsx
@@ -3,7 +3,7 @@ import { Rating } from '../../../../src'
 
 const RatingTypes = () => {
   return (
-    <Rating size={30}>
+    <Rating size={30} className="space-x-1">
       <Rating.Star starType="full" filledType="fill" />
       <Rating.Star starType="half" filledType="fill" />
     </Rating>
@@ -11,16 +11,16 @@ const RatingTypes = () => {
 }
 
 const RatingTypesCode = `
-"use client";
-import { Rating } from "keep-react";
+'use client'
+import { Rating } from 'keep-react'
 
 export const RatingComponent = () => {
   return (
-    <Rating size={30}>
+    <Rating size={30} className="space-x-1">
       <Rating.Star starType="full" filledType="fill" />
       <Rating.Star starType="half" filledType="fill" />
     </Rating>
-  );
+  )
 }
 `
 
