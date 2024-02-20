4c3428a53915d24ef0500b9d5cb04d430ef19c91
Arifulislam5577 <arifalamm33@gmail.com>
2024-02-20 21:46:43 +0600

    Fixed: Topbar issue fixed & discord new link updated.

--- app/components/TopProgressBar.tsx
+++ app/components/TopProgressBar.tsx
@@ -1,13 +1,12 @@
 'use client'
-
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
-import React from 'react'
+import { Suspense } from 'react'
 
 const TopProgressBar = () => {
   return (
-    <>
+    <Suspense>
       <ProgressBar height="2px" color="#1B4DFF" options={{ showSpinner: false }} />
-    </>
+    </Suspense>
   )
 }
 
