76e6d5b32d620945c455fbcc1415e2a4862d2d06
Arifulislam5577 <arifalamm33@gmail.com>
2024-03-11 14:03:04 +0600

    Fixed: Checkbox issue fixed.

--- app/src/components/CheckBox/Checkbox.tsx
+++ app/src/components/CheckBox/Checkbox.tsx
@@ -7,31 +7,31 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   variant?: 'circle' | 'rounded' | 'checked' | 'dashed'
 }
 
-export const Checkbox = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, variant = 'checked', ...props }, ref) => {
-    let btnClass = cn(checkboxTheme.button.base, className)
-    if (variant === 'rounded' || variant === 'circle') {
-      btnClass = cn(checkboxTheme.button.shape.base, checkboxTheme.button.shape[variant], className)
-    }
+const Checkbox = forwardRef<HTMLInputElement, InputProps>(({ className, variant = 'checked', ...props }, ref) => {
+  let btnClass = cn(checkboxTheme.button.base, className)
+  if (variant === 'rounded' || variant === 'circle') {
+    btnClass = cn(checkboxTheme.button.shape.base, checkboxTheme.button.shape[variant], className)
+  }
 
-    return (
-      <span>
-        <input ref={ref} {...props} type="checkbox" className={cn(checkboxTheme.input.base)} />
-        <button className={btnClass}>
-          {variant === 'checked' && (
-            <svg width={12} height={12} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
-              <path d="M232.49,80.49l-128,128a12,12,0,0,1-17,0l-56-56a12,12,0,1,1,17-17L96,183,215.51,63.51a12,12,0,0,1,17,17Z"></path>
-            </svg>
-          )}
-          {variant === 'dashed' && (
-            <svg xmlns="http://www.w3.org/2000/svg" width={12} height={12} viewBox="0 0 256 256">
-              <path d="M228,128a12,12,0,0,1-12,12H40a12,12,0,0,1,0-24H216A12,12,0,0,1,228,128Z"></path>
-            </svg>
-          )}
-        </button>
-      </span>
-    )
-  },
-)
+  return (
+    <span>
+      <input ref={ref} {...props} type="checkbox" className={cn(checkboxTheme.input.base)} />
+      <button className={btnClass}>
+        {variant === 'checked' && (
+          <svg width={12} height={12} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+            <path d="M232.49,80.49l-128,128a12,12,0,0,1-17,0l-56-56a12,12,0,1,1,17-17L96,183,215.51,63.51a12,12,0,0,1,17,17Z"></path>
+          </svg>
+        )}
+        {variant === 'dashed' && (
+          <svg xmlns="http://www.w3.org/2000/svg" width={12} height={12} viewBox="0 0 256 256">
+            <path d="M228,128a12,12,0,0,1-12,12H40a12,12,0,0,1,0-24H216A12,12,0,0,1,228,128Z"></path>
+          </svg>
+        )}
+      </button>
+    </span>
+  )
+})
 
 Checkbox.displayName = 'Checkbox'
+
+export { Checkbox }
