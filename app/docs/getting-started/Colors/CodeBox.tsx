9db70f157c6e20b5cbac8f678f6ee31ef745eb14
Arifulislam5577 <arifalamm33@gmail.com>
2024-03-27 13:16:23 +0600

    Added: color page added.

--- app/docs/getting-started/Colors/CodeBox.tsx
+++ app/docs/getting-started/Colors/CodeBox.tsx
@@ -16,10 +16,7 @@ const CodeBox: FC<CodeBoxProps> = ({ color, id, code }) => {
     <div>
       <button
         onClick={() => copyToClipboard(code)}
-        className={cn(
-          'group flex h-10 w-16 border-spacing-2 items-center justify-center rounded-lg border-2 border-metal-200',
-          color,
-        )}>
+        className={cn('group flex h-10 w-16 border-spacing-2 items-center justify-center rounded-lg', color)}>
         <span
           className={cn(
             'opacity-0 transition-all duration-300 group-hover:opacity-100',
@@ -28,9 +25,9 @@ const CodeBox: FC<CodeBoxProps> = ({ color, id, code }) => {
           {copy ? <Check size={18} /> : <Copy size={18} />}
         </span>
       </button>
-      <div className="text-center">
-        <p className="text-body-5">{id}</p>
-        <p className="text-[10px]">{code}</p>
+      <div className="mt-1 text-left">
+        <p className="text-[10px] font-medium text-metal-600">{id}</p>
+        <p className="text-[10px] font-medium text-metal-600">{code}</p>
       </div>
     </div>
   )
