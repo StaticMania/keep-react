61df3e32fb259d0d0346d92f1fb8000fcee36c88
writeeasyjs <shuvo.devhub@gmail.com>
2024-04-06 22:29:29 +0600

    Update: Docs for classNames option in carousel.

--- app/docs/components/carousel/variant/ClassNames.tsx
+++ app/docs/components/carousel/variant/ClassNames.tsx
@@ -1,58 +1,69 @@
 'use client'
-import React from 'react'
-import Carousel from '~/app/src/components/Carousel/Carousel'
+import Image, { StaticImageData } from 'next/image'
 import { default as classNames } from 'embla-carousel-class-names'
-import Image from 'next/image'
+import { Carousel } from '~/app/src'
+import imgSrc1 from '/public/images/carousel/1.jpg'
+import imgSrc2 from '/public/images/carousel/2.jpg'
+import imgSrc3 from '/public/images/carousel/3.jpg'
+import imgSrc4 from '/public/images/carousel/4.jpg'
+import imgSrc5 from '/public/images/carousel/5.jpg'
 
-const ClassNames = () => {
-  const images = [
-    '/images/carousel/1.jpg',
-    '/images/carousel/2.jpg',
-    '/images/carousel/3.jpg',
-    '/images/carousel/4.jpg',
-    '/images/carousel/5.jpg',
-  ]
+const images: Array<StaticImageData> = [imgSrc1, imgSrc2, imgSrc3, imgSrc4, imgSrc5]
 
-  const slides = images.map((src, idx) => (
-    <div className="b relative h-[400px] w-full overflow-hidden rounded-lg" key={idx}>
-      <Image fill src={src} alt={'img' + idx} className="w-full object-cover" />
-    </div>
-  ))
-  return (
-    <Carousel
-      slides={slides}
-      options={{ loop: true }}
-      slideClass="flex-[0_0_70%]  [&:not(.is-snapped)]:opacity-[0.16]"
-      carouselPlugins={[classNames()]}
-    />
-  )
-}
+const ClassNames = () => (
+  <Carousel options={{ loop: true }} carouselPlugins={[classNames()]}>
+    <Carousel.Slides>
+      {images.map((src, index) => (
+        <Carousel.Item key={index} slideClass="flex-[0_0_70%]  [&:not(.is-snapped)]:opacity-[0.16]">
+          <div className="b relative h-[400px] w-full overflow-hidden rounded-lg">
+            <Image fill src={src} className="w-full object-cover" alt="carousel img" />
+          </div>
+        </Carousel.Item>
+      ))}
+    </Carousel.Slides>
+    <Carousel.Control>
+      <Carousel.Buttons>
+        <Carousel.PrevButton />
+        <Carousel.NextButton />
+      </Carousel.Buttons>
+      <Carousel.Indicators />
+    </Carousel.Control>
+  </Carousel>
+)
 
 const ClassNamesCode = `
 'use client'
-import React from 'react'
-import Carousel from '~/app/src/components/Carousel/Carousel'
+import Image, { StaticImageData } from 'next/image'
 import { default as classNames } from 'embla-carousel-class-names'
-import Image from 'next/image'
+import { Carousel } from '~/app/src'
+import imgSrc1 from '/public/images/carousel/1.jpg'
+import imgSrc2 from '/public/images/carousel/2.jpg'
+import imgSrc3 from '/public/images/carousel/3.jpg'
+import imgSrc4 from '/public/images/carousel/4.jpg'
+import imgSrc5 from '/public/images/carousel/5.jpg'
 
-const ClassNames = () => {
-  const images = [
-    '/images/carousel/1.jpg',
-    '/images/carousel/2.jpg',
-    '/images/carousel/3.jpg',
-    '/images/carousel/4.jpg',
-    '/images/carousel/5.jpg',
-  ]
+const images: Array<StaticImageData> = [imgSrc1, imgSrc2, imgSrc3, imgSrc4, imgSrc5]
 
-  const slides = images.map((src, idx) => (
-    <div className="b relative h-[400px] w-full overflow-hidden rounded-lg" key={idx}>
-      <Image fill src={src} alt={'img' + idx} className="w-full object-cover" />
-    </div>
-  ))
-  return (
-    <Carousel slides={slides} options={{ loop: true }} slideClass="flex-[0_0_70%]" carouselPlugins={[classNames()]} />
-  )
-}
+const ClassNames = () => (
+  <Carousel options={{ loop: true }} carouselPlugins={[classNames()]}>
+    <Carousel.Slides>
+      {images.map((src, index) => (
+        <Carousel.Item key={index} slideClass="flex-[0_0_70%]  [&:not(.is-snapped)]:opacity-[0.16]">
+          <div className="b relative h-[400px] w-full overflow-hidden rounded-lg">
+            <Image fill src={src} className="w-full object-cover" alt="carousel img" />
+          </div>
+        </Carousel.Item>
+      ))}
+    </Carousel.Slides>
+    <Carousel.Control>
+      <Carousel.Buttons>
+        <Carousel.PrevButton />
+        <Carousel.NextButton />
+      </Carousel.Buttons>
+      <Carousel.Indicators />
+    </Carousel.Control>
+  </Carousel>
+)
 `
 
 export { ClassNames, ClassNamesCode }
