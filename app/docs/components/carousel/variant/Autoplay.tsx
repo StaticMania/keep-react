4716107338751e7db2c6cac42d6e899770122177
writeeasyjs <shuvo.devhub@gmail.com>
2024-04-06 17:28:11 +0600

    Update: Autoplay option variant in carousel docs.

--- app/docs/components/carousel/variant/Autoplay.tsx
+++ app/docs/components/carousel/variant/Autoplay.tsx
@@ -1,63 +1,69 @@
 'use client'
-import Image from 'next/image'
-import React from 'react'
-import Carousel from '~/app/src/components/Carousel/Carousel'
+import Image, { StaticImageData } from 'next/image'
 import { default as autoPlay } from 'embla-carousel-autoplay'
+import { Carousel } from '~/app/src'
+import imgSrc1 from '/public/images/carousel/1.jpg'
+import imgSrc2 from '/public/images/carousel/2.jpg'
+import imgSrc3 from '/public/images/carousel/3.jpg'
+import imgSrc4 from '/public/images/carousel/4.jpg'
+import imgSrc5 from '/public/images/carousel/5.jpg'
 
-const Autoplay = () => {
-  const images = [
-    '/images/carousel/1.jpg',
-    '/images/carousel/2.jpg',
-    '/images/carousel/3.jpg',
-    '/images/carousel/4.jpg',
-    '/images/carousel/5.jpg',
-  ]
+const images: Array<StaticImageData> = [imgSrc1, imgSrc2, imgSrc3, imgSrc4, imgSrc5]
 
-  const slides = images.map((src, idx) => (
-    <div className="b relative h-[400px] w-full overflow-hidden rounded-lg" key={idx}>
-      <Image fill src={src} alt={'img' + idx} className="w-full object-cover" />
-    </div>
-  ))
-  return (
-    <Carousel
-      slides={slides}
-      options={{ loop: true }}
-      slideClass="flex-[0_0_50%]"
-      carouselPlugins={[autoPlay({ playOnInit: true, delay: 3000 })]}
-    />
-  )
-}
+const Autoplay = () => (
+  <Carousel options={{ loop: true }} carouselPlugins={[autoPlay()]}>
+    <Carousel.Slides>
+      {images.map((src, index) => (
+        <Carousel.Item key={index} slideClass="flex-[0_0_50%]">
+          <div className="b relative h-[400px] w-full overflow-hidden rounded-lg">
+            <Image fill src={src} className="w-full object-cover" alt="carousel img" />
+          </div>
+        </Carousel.Item>
+      ))}
+    </Carousel.Slides>
+    <Carousel.Control>
+      <Carousel.Buttons>
+        <Carousel.PrevButton />
+        <Carousel.NextButton />
+      </Carousel.Buttons>
+      <Carousel.Indicators />
+    </Carousel.Control>
+  </Carousel>
+)
 
 const AutoplayCode = `
 'use client'
-import Image from 'next/image'
-import React from 'react'
-import Carousel from '~/app/src/components/Carousel/Carousel'
+import Image, { StaticImageData } from 'next/image'
 import { default as autoPlay } from 'embla-carousel-autoplay'
+import { Carousel } from '~/app/src'
+import imgSrc1 from '/public/images/carousel/1.jpg'
+import imgSrc2 from '/public/images/carousel/2.jpg'
+import imgSrc3 from '/public/images/carousel/3.jpg'
+import imgSrc4 from '/public/images/carousel/4.jpg'
+import imgSrc5 from '/public/images/carousel/5.jpg'
 
-const Autoplay = () => {
-  const images = [
-    '/images/carousel/1.jpg',
-    '/images/carousel/2.jpg',
-    '/images/carousel/3.jpg',
-    '/images/carousel/4.jpg',
-    '/images/carousel/5.jpg',
-  ]
+const images: Array<StaticImageData> = [imgSrc1, imgSrc2, imgSrc3, imgSrc4, imgSrc5]
 
-  const slides = images.map((src, idx) => (
-    <div className="b relative h-[400px] w-full overflow-hidden rounded-lg" key={idx}>
-      <Image fill src={src} alt={'img' + idx} className="w-full object-cover" />
-    </div>
-  ))
-  return (
-    <Carousel
-      slides={slides}
-      options={{ loop: true }}
-      slideClass="flex-[0_0_50%]"
-      carouselPlugins={[autoPlay({ playOnInit: true, delay: 3000 })]}
-    />
-  )
-}
+const Autoplay = () => (
+  <Carousel options={{ loop: true }} carouselPlugins={[autoPlay()]}>
+    <Carousel.Slides>
+      {images.map((src, index) => (
+        <Carousel.Item key={index} slideClass="flex-[0_0_50%]">
+          <div className="b relative h-[400px] w-full overflow-hidden rounded-lg">
+            <Image fill src={src} className="w-full object-cover" alt="carousel img" />
+          </div>
+        </Carousel.Item>
+      ))}
+    </Carousel.Slides>
+    <Carousel.Control>
+      <Carousel.Buttons>
+        <Carousel.PrevButton />
+        <Carousel.NextButton />
+      </Carousel.Buttons>
+      <Carousel.Indicators />
+    </Carousel.Control>
+  </Carousel>
+)
 `
 
 export { Autoplay, AutoplayCode }
