995b7e1f5dbdbb2bab0cec643b5d716682433271
shuvo-me <shuvo.js.dev@gmail.com>
2024-05-13 14:51:02 +0600

    Setup: Props for linechart.

--- app/src/components/Chart/LineChart.tsx
+++ app/src/components/Chart/LineChart.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import {
   LineChart as LineCartCom,
   Line,
@@ -11,6 +11,9 @@ import {
   ResponsiveContainer,
   BarProps,
 } from 'recharts'
+import { CustomTooltip } from './CustomTooltip'
+import CustomLegend from './CustomLegend'
+import { ChartContext } from './ChartContext'
 
 interface LineChartProps extends BarProps {
   tooltipBtnColor?: 'primary' | 'secondary' | 'success' | 'warning' | 'error'
@@ -19,7 +22,7 @@ interface LineChartProps extends BarProps {
   activeColor?: string
   activeIndex?: number
   barBg?: string
-  barColor?: string
+  lineColor?: string
   barRadius?: number | [number, number, number, number]
   barSize?: number
   chartData?: unknown[]
@@ -27,7 +30,7 @@ interface LineChartProps extends BarProps {
   height?: number
   inActiveColor?: string
   secondaryBarBg?: string
-  secondaryBarColor?: string
+  secondaryLineColor?: string
   secondaryDataKey?: string
   showBg?: boolean
   showGridLine?: boolean
@@ -43,42 +46,72 @@ interface LineChartProps extends BarProps {
 const LineChart: FC<LineChartProps> = ({
   showGridLine = false,
   showTooltip = false,
-  showBg = false,
+  //   showBg = false,
   showLegend = false,
-  active = false,
-  activeColor,
-  inActiveColor,
-  barColor = '#1C222B',
-  barBg = '#F0F3F9',
-  secondaryBarColor = '#3D4A5C',
-  secondaryBarBg = '#F0F3F9',
-  activeIndex = 4,
+  //   active = false,
+  //   activeColor,
+  //   inActiveColor,
+  lineColor = '#1C222B',
+  //   barBg = '#F0F3F9',
+  secondaryLineColor = '#3D4A5C',
+  //   secondaryBarBg = '#F0F3F9',
+  //   activeIndex = 4,
   XAxisDataKey,
   dataKey,
   YAxisDataKey,
   chartData,
-  barRadius = [8, 8, 0, 0],
   height = 300,
   width = 600,
-  barSize = 32,
   secondaryDataKey,
   tooltipBtnColor = 'primary',
   tooltipBtnStyle,
   showXAxis = false,
   showYAxis = false,
 }) => {
-  return (
+  const [client, setClient] = useState(() => {
+    console.log('before use effect')
+    return false
+  })
+  const RenderLineCart = (
     <ResponsiveContainer width="100%" height="100%">
       <LineCartCom width={width} height={height} data={chartData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        {showGridLine && <CartesianGrid strokeDasharray="4 4" fill="#E9EFF6" y={1000} />}
+        {showXAxis && (
+          <XAxis
+            className="text-body-4 font-medium text-metal-600"
+            axisLine={false}
+            dataKey={XAxisDataKey}
+            strokeDasharray="4 4"
+            tickSize={20}
+            tickLine={false}
+          />
+        )}
+
+        {showYAxis && (
+          <YAxis
+            axisLine={false}
+            className="text-body-4 font-medium text-metal-600"
+            dataKey={YAxisDataKey}
+            strokeDasharray="4 4"
+            tickLine={false}
+            tickSize={10}
+          />
+        )}
+        {showTooltip && <Tooltip cursor={false} offset={10} content={<CustomTooltip />} />}
+        {showLegend && <Legend verticalAlign="top" align="left" iconSize={6} content={<CustomLegend payload={[]} />} />}
+        <Line type="monotone" dataKey={dataKey} stroke={lineColor} activeDot={{ r: 8 }} />
+        {secondaryDataKey && <Line type="monotone" dataKey={secondaryDataKey} stroke={secondaryLineColor} />}
       </LineCartCom>
     </ResponsiveContainer>
+  )
+  useEffect(() => {
+    console.log('after mounted')
+    setClient(true)
+  }, [])
+  return (
+    <ChartContext.Provider value={{ tooltipBtnColor, tooltipBtnStyle }}>
+      {client ? RenderLineCart : null}
+    </ChartContext.Provider>
   )
 }
 
