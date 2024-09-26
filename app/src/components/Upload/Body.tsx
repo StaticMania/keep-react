e6ed0cb4959c0b41091bbc981f9a7da42ed06071
saiful7778 <saiful.islam.rafi.88@gmail.com>
2024-09-26 09:24:20 +0600

    Feat(Upload): Add Drag active and Drag reject effect.

--- app/src/components/Upload/Body.tsx
+++ app/src/components/Upload/Body.tsx
@@ -7,16 +7,17 @@ import { useUploadContext } from './UploadContext'
 const UploadBody = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
   ({ children, className, ...props }, ref: Ref<HTMLDivElement>) => {
     const { options, horizontal } = useUploadContext()
-    const { getRootProps, getInputProps } = useDropzone(options)
+    const { getRootProps, getInputProps, isDragActive, isDragReject } = useDropzone(options)
     return (
       <div
         {...props}
         ref={ref}
         {...getRootProps()}
         className={cn(
-          horizontal
-            ? 'flex cursor-pointer items-center gap-4 rounded-xl border border-dashed bg-white px-3 transition-all duration-300 hover:bg-metal-25 dark:border-metal-800 dark:bg-metal-900 dark:hover:bg-metal-900'
-            : 'flex cursor-pointer flex-col items-center rounded-xl border border-dashed border-metal-50 bg-white p-3 text-center transition-all duration-300 hover:bg-metal-25 dark:border-metal-800 dark:bg-metal-900 dark:hover:bg-metal-900',
+          'flex cursor-pointer items-center rounded-xl border border-dashed bg-white transition-all duration-300 hover:bg-metal-25 dark:border-metal-800 dark:bg-metal-900 dark:hover:bg-metal-900',
+          horizontal ? 'gap-4 px-3' : 'flex-col border-metal-50 p-3 text-center',
+          isDragActive && 'border-blue-500 bg-sky-100 dark:bg-blue-900',
+          isDragReject && 'border-red-500 bg-red-100 dark:bg-red-900',
           className,
         )}>
         <input {...getInputProps()} />
