8a0036f797b0dfdc5f2d75a98f10f72ff1045dfc
saiful7778 <saiful.islam.rafi.88@gmail.com>
2024-12-31 20:36:13 +0600

    Added: Radix slot in button component.

--- app/src/components/Button/Button.tsx
+++ app/src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { ButtonHTMLAttributes, Ref, forwardRef } from 'react'
+import { Slot } from '@radix-ui/react-slot'
 import { cn } from '../../utils/cn'
 import { ButtonColorVariant, ButtonSizeVariant, buttonVariants } from './theme'
 
@@ -9,6 +10,7 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'link' | 'outline' | 'softBg' | 'default'
   shape?: 'circle' | 'icon'
   position?: 'start' | 'end' | 'center'
+  asChild?: boolean
   radius?: 'default' | 'full'
 }
 
@@ -22,18 +24,20 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       variant = 'default',
       radius = 'default',
       shape,
+      asChild,
       position,
       ...props
     },
     ref: Ref<HTMLButtonElement>,
   ) => {
+    const Comp = asChild ? Slot : 'button'
     return (
-      <button
+      <Comp
         {...props}
         ref={ref}
         className={cn(buttonVariants({ size, color, variant, shape, position, radius }), className)}>
         {children}
-      </button>
+      </Comp>
     )
   },
 )
